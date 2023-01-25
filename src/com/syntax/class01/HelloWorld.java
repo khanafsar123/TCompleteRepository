e4623206c61099e0b66bb2ebcf55090db9ad9d65
Khan Afsar <khanafsar456@gmail.com>
2023-01-25 07:55:11 -0800

    All done

--- src/com/syntax/class01/HelloWorld.java
+++ src/com/syntax/class01/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Batch 14");
-		System.out.println(20);
+		System.out.println(30);
 		System.out.println("My Name is Talha ");
 	}
 
